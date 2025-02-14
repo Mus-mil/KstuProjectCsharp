03cf180a4bd86b7b34c94be86fceb9b0f99865d5
mus-mil <aminsabirov2006@gmail.com>
2025-02-14 09:26:46 +0300

    delete idea

---
+++ Program_0_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KstuProjectCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1;
+            int numNegative = -1;
+            float num3 = 3.14f;
+            float num4 = 4.56f;
+            bool flag = false;
+            bool flagName = false;
+            byte num256 = 255;
+            byte numNil = 0;
+            string numNilStr = "";
+            string numHelloWorld= "Hello World";
+        }
+    }
+}
